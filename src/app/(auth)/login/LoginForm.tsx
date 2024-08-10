50a2817031f1bcc2306f0b643c6532e97f0ca10a
Pikulkaew Boonpeng <pkboonpeng@gmail.com>
2024-08-10 18:50:28 -0400

    auth actions

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,83 +1,107 @@
 "use client";
 import {
-  Button,
-  Card,
-  CardContent,
-  Stack,
-  TextField,
-  Typography,
+    Button,
+    Card,
+    CardContent,
+    CircularProgress,
+    Stack,
+    TextField,
+    Typography,
 } from "@mui/material";
 import { Lock } from "@mui/icons-material";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { LoginSchema, loginSchema } from "@/lib/schemas/loginSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { signInUser } from "../actions/authActions";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm<LoginSchema>({
-    resolver: zodResolver(loginSchema),
-    mode: "onTouched",
-  });
+    const router = useRouter();
 
-  const onLoginSubmit = (data: LoginSchema) => {
-    console.log(data);
-  };
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isValid, isSubmitting },
+    } = useForm<LoginSchema>({
+        resolver: zodResolver(loginSchema),
+        mode: "onTouched",
+    });
 
-  return (
-    <Card className="w-2/5 mx-auto h-min rounded-xl">
-      <CardContent>
-        <Stack
-          direction="row"
-          className="items-center mb-1 gap-1 justify-center"
-        >
-          <Lock className="text-rose-600 text-xl" />
-          <Typography variant="h5" className="text-rose-600 font-bold">
-            Login
-          </Typography>
-        </Stack>
-        <Typography className="text-slate-400 text-center">
-          Welcome back to NextMatch
-        </Typography>
+    const onLoginSubmit = async (data: LoginSchema) => {
+        const result = await signInUser(data);
+        if (result.status === "success") {
+            router.push("/members");
+        } else {
+            console.log(result.error);
+        }
+    };
 
-        <form
-          className="mt-3 grid gap-3"
-          onSubmit={handleSubmit(onLoginSubmit)}
-        >
-          <TextField
-            type="email"
-            color="error"
-            variant="outlined"
-            label="Email"
-            defaultValue=""
-            {...register("email")}
-            error={!!errors.email}
-            helperText={errors.email?.message}
-          />
-          <TextField
-            type="password"
-            color="error"
-            variant="outlined"
-            label="Password"
-            defaultValue=""
-            {...register("password")}
-            error={!!errors.password}
-            helperText={errors.password?.message}
-          />
-          <Button
-            type="submit"
-            color="error"
-            variant="contained"
-            size="large"
-            disabled={!isValid}
-          >
-            Log in
-          </Button>
-        </form>
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card className="w-2/5 mx-auto h-min rounded-xl">
+            <CardContent>
+                <Stack
+                    direction="row"
+                    className="items-center mb-1 gap-1 justify-center"
+                >
+                    <Lock className="text-rose-600 text-xl" />
+                    <Typography
+                        variant="h5"
+                        className="text-rose-600 font-bold"
+                    >
+                        Login
+                    </Typography>
+                </Stack>
+                <Typography className="text-slate-400 text-center">
+                    Welcome back to NextMatch
+                </Typography>
+
+                <form
+                    className="mt-3 grid gap-3"
+                    onSubmit={handleSubmit(onLoginSubmit)}
+                >
+                    <TextField
+                        type="email"
+                        color="error"
+                        variant="outlined"
+                        label="Email"
+                        defaultValue=""
+                        {...register("email")}
+                        error={!!errors.email}
+                        helperText={errors.email?.message}
+                    />
+                    <TextField
+                        type="password"
+                        color="error"
+                        variant="outlined"
+                        label="Password"
+                        defaultValue=""
+                        {...register("password")}
+                        error={!!errors.password}
+                        helperText={errors.password?.message}
+                    />
+                    <Button
+                        startIcon={
+                            isSubmitting && (
+                                <CircularProgress
+                                    className="text-white opacity-75"
+                                    style={{
+                                        width: "1.25rem",
+                                        height: "1.25rem",
+                                    }}
+                                />
+                            )
+                        }
+                        type="submit"
+                        color="error"
+                        variant="contained"
+                        size="large"
+                        disabled={!isValid}
+                    >
+                        Log in
+                    </Button>
+                </form>
+            </CardContent>
+        </Card>
+    );
 }
--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -1,93 +1,131 @@
 "use client";
 import {
-  Button,
-  Card,
-  CardContent,
-  Stack,
-  TextField,
-  Typography,
+    Button,
+    Card,
+    CardContent,
+    CircularProgress,
+    Stack,
+    TextField,
+    Typography,
 } from "@mui/material";
 import { Person } from "@mui/icons-material";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { RegisterSchema, registerSchema } from "@/lib/schemas/registerSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { registerUser } from "../actions/authActions";
 
 export default function RegisterForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm<RegisterSchema>({
-    resolver: zodResolver(registerSchema),
-    mode: "onTouched",
-  });
+    const {
+        register,
+        setError,
+        handleSubmit,
+        formState: { errors, isValid, isSubmitting },
+    } = useForm<RegisterSchema>({
+        resolver: zodResolver(registerSchema),
+        mode: "onTouched",
+    });
 
-  const onRegisterSubmit = (data: RegisterSchema) => {
-    console.log(data);
-  };
+    const onRegisterSubmit = async (data: RegisterSchema) => {
+        const result = await registerUser(data);
 
-  return (
-    <Card className="w-2/5 mx-auto h-min rounded-xl">
-      <CardContent>
-        <Stack
-          direction="row"
-          className="items-center mb-1 gap-1 justify-center"
-        >
-          <Person className="text-rose-600 text-2xl" />
-          <Typography variant="h5" className="text-rose-600 font-bold">
-            Register
-          </Typography>
-        </Stack>
-        <Typography className="text-slate-400 text-center">
-          Welcome to NextMatch
-        </Typography>
+        if (result.status === "success") {
+            console.log("User registered succesfully");
+        } else {
+            if (Array.isArray(result.error)) {
+                result.error.forEach((e) => {
+                    const fieldName = e.path.join(". ") as
+                        | "email"
+                        | "name"
+                        | "password";
+                    setError(fieldName, { message: e.message });
+                });
+            } else {
+                setError("root.serverError", { message: result.error });
+            }
+        }
+    };
 
-        <form
-          className="mt-3 grid gap-3"
-          onSubmit={handleSubmit(onRegisterSubmit)}
-        >
-          <TextField
-            type="text"
-            color="error"
-            variant="outlined"
-            label="Name"
-            defaultValue=""
-            {...register("name")}
-            error={!!errors.name}
-            helperText={errors.name?.message}
-          />
-          <TextField
-            type="email"
-            color="error"
-            variant="outlined"
-            label="Email"
-            defaultValue=""
-            {...register("email")}
-            error={!!errors.email}
-            helperText={errors.email?.message}
-          />
-          <TextField
-            type="password"
-            color="error"
-            variant="outlined"
-            label="Password"
-            defaultValue=""
-            {...register("password")}
-            error={!!errors.password}
-            helperText={errors.password?.message}
-          />
-          <Button
-            type="submit"
-            color="error"
-            variant="contained"
-            size="large"
-            disabled={!isValid}
-          >
-            Register
-          </Button>
-        </form>
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card className="w-2/5 mx-auto h-min rounded-xl">
+            <CardContent>
+                <Stack
+                    direction="row"
+                    className="items-center mb-1 gap-1 justify-center"
+                >
+                    <Person className="text-rose-600 text-2xl" />
+                    <Typography
+                        variant="h5"
+                        className="text-rose-600 font-bold"
+                    >
+                        Register
+                    </Typography>
+                </Stack>
+                <Typography className="text-slate-400 text-center">
+                    Welcome to NextMatch
+                </Typography>
+
+                <form
+                    className="mt-3 grid gap-3"
+                    onSubmit={handleSubmit(onRegisterSubmit)}
+                >
+                    <TextField
+                        type="text"
+                        color="error"
+                        variant="outlined"
+                        label="Name"
+                        defaultValue=""
+                        {...register("name")}
+                        error={!!errors.name}
+                        helperText={errors.name?.message}
+                    />
+                    <TextField
+                        type="email"
+                        color="error"
+                        variant="outlined"
+                        label="Email"
+                        defaultValue=""
+                        {...register("email")}
+                        error={!!errors.email}
+                        helperText={errors.email?.message}
+                    />
+                    <TextField
+                        type="password"
+                        color="error"
+                        variant="outlined"
+                        label="Password"
+                        defaultValue=""
+                        {...register("password")}
+                        error={!!errors.password}
+                        helperText={errors.password?.message}
+                    />
+                    {errors.root?.serverError && (
+                        <p className="text-red-500 text-sm">
+                            {errors.root.serverError.message}
+                        </p>
+                    )}
+                    <Button
+                        startIcon={
+                            isSubmitting && (
+                                <CircularProgress
+                                    className="text-white opacity-75"
+                                    style={{
+                                        width: "1.25rem",
+                                        height: "1.25rem",
+                                    }}
+                                />
+                            )
+                        }
+                        type="submit"
+                        color="error"
+                        variant="contained"
+                        size="large"
+                        disabled={!isValid}
+                    >
+                        Register
+                    </Button>
+                </form>
+            </CardContent>
+        </Card>
+    );
 }
